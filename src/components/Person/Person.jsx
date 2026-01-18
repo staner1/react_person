29a2264660f32e370712f0952178fd72065f2a47
Oleksandr <kozyrev.sania@gmail.com>
2026-01-18 14:23:10 +0200

    create const partnerTitle

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -11,8 +11,10 @@ export const Person = ({ person }) => {
     let result = <p className="Person__partner">I am not married</p>;
 
     if (isMarried === true) {
+      const partnerTitle = sex === 'm' ? 'wife' : 'husband';
+
       result = (
-        <p className="Person__partner">{`${partnerName} is my ${sex === 'm' ? 'wife' : 'husband'}`}</p>
+        <p className="Person__partner">{`${partnerName} is my ${partnerTitle}`}</p>
       );
     }
 
